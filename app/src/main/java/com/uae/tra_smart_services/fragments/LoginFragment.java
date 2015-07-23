5d930dcf70203c4379c423aa153f3c8254d67193
andreykorney <akbuffalo1@gmail.com>
2015-07-23 13:07:07 +0300

    Registration UI partially implemented. Need the 'Forgot password' screen implementation.

--- app/src/main/java/com/uae/tra_smart_services/fragments/LoginFragment.java
+++ app/src/main/java/com/uae/tra_smart_services/fragments/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.uae.tra_smart_services.fragments;
 
+import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
