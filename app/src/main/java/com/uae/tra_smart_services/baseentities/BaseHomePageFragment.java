ac5261823b798b09189e75b048fe74e6b08c2ca6
andreykorney <akbuffalo1@gmail.com>
2015-07-30 18:03:54 +0300

    Some refactoring

---
+++ app/src/main/java/com/uae/tra_smart_services/baseentities/BaseHomePageFragment.java
@@ -0,0 +1,7 @@
+package com.uae.tra_smart_services.baseentities;
+
+/**
+ * Created by ak-buffalo on 30.07.15.
+ */
+public class BaseHomePageFragment {
+}
---
+++ app/src/main/java/com/uae/tra_smart_services/fragments/SettingsFragment.java
@@ -0,0 +1,7 @@
+package com.uae.tra_smart_services.fragments;
+
+/**
+ * Created by ak-buffalo on 30.07.15.
+ */
+public class SettingsFragment {
+}
