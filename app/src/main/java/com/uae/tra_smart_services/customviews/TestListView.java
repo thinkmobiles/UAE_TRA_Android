cabdab3cf52419cdd503813a7ce838afde0d97da
Andrey Korneychuk <akbuffalo1@gmail.com>
2015-08-26 11:14:32 +0300

    adasd

---
+++ app/src/main/java/com/uae/tra_smart_services/customviews/TestListView.java
@@ -0,0 +1,20 @@
+package com.uae.tra_smart_services.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ListView;
+
+/**
+ * Created by ak-buffalo on 25.08.15.
+ */
+public class TestListView extends ListView {
+    public TestListView(Context context) {
+        super(context);
+    }
+
+    public TestListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
