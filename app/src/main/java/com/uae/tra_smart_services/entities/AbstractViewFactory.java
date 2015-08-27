a9dccd9938fcf96c635181653b40da262b93ddc8
akbuffalo1 <zkexibq1>
2015-08-27 23:53:11 +0300

    refactored  interface "Abstractviewfactory"name

--- app/src/main/java/com/uae/tra_smart_services/entities/AbstractViewFactory.java
+++ app/src/main/java/com/uae/tra_smart_services/entities/AbstractViewFactory.java
@@ -3,10 +3,12 @@ package com.uae.tra_smart_services.entities;
 import android.content.Context;
 import android.view.View;
 
+import com.uae.tra_smart_services.interfaces.ViewFactory;
+
 /**
  * Created by Andrey Korneychuk on 7/26/15.
  */
-public abstract class AbstractViewFactory<T extends View,K> implements com.uae.tra_smart_services.interfaces.AbstractViewFactory<T,K> {
+public abstract class AbstractViewFactory<T extends View,K> implements ViewFactory<T,K> {
 	protected Context mContext;
 
 	protected AbstractViewFactory(Context _context){
