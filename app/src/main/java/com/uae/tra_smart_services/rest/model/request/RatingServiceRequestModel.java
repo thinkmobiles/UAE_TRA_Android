55300e9365ad097a63920db9dd1b7acc29f4b978
Andrey Korneychuk <akbuffalo1@gmail.com>
2015-10-03 14:41:08 +0300

    Small refactoring

--- app/src/main/java/com/uae/tra_smart_services/rest/model/request/RatingServiceRequestModel.java
+++ app/src/main/java/com/uae/tra_smart_services/rest/model/request/RatingServiceRequestModel.java
@@ -19,34 +19,34 @@ public class RatingServiceRequestModel{
         this(_serviceName, _rate, null);
     }
 
-    public RatingServiceRequestModel(String _serviceName, Integer _rate, String feedback) {
+    public RatingServiceRequestModel(String _serviceName, Integer _rate, String _feedback) {
         serviceName = _serviceName;
         rate = _rate;
-        feedback = (feedback == null) ? "" : feedback;
+        feedback = (_feedback == null) ? String.valueOf(_rate) : _feedback;
     }
 
     public String getServiceName() {
         return serviceName;
     }
 
-    public void setServiceName(String serviceName) {
-        this.serviceName = serviceName;
+    public void setServiceName(String _serviceName) {
+        serviceName = _serviceName;
     }
 
     public int getRate() {
         return rate;
     }
 
-    public void setRate(int rate) {
-        this.rate = rate;
+    public void setRate(int _rate) {
+        this.rate = _rate;
     }
 
     public String getFeedback() {
         return feedback;
     }
 
-    public void setFeedback(String feedback) {
-        this.feedback = feedback;
+    public void setFeedback(String _feedback) {
+        feedback = _feedback;
     }
 
     @Override
