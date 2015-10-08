483c5ecc417b121bcc74ff118b360de660a7f5c5
mobimaks <mobimaks13@gmail.com>
2015-10-08 12:16:13 +0300

    Add profile url to model; Change "favourites" to "favorites"

--- app/src/main/java/com/uae/tra_smart_services/rest/robo_requests/ChangeUserNameRequest.java
+++ app/src/main/java/com/uae/tra_smart_services/rest/robo_requests/ChangeUserNameRequest.java
@@ -1,6 +1,5 @@
 package com.uae.tra_smart_services.rest.robo_requests;
 
-import com.uae.tra_smart_services.entities.UserProfile;
 import com.uae.tra_smart_services.rest.TRAServicesAPI;
 import com.uae.tra_smart_services.rest.model.request.UserNameModel;
 import com.uae.tra_smart_services.rest.model.response.UserProfileResponseModel;
@@ -15,16 +14,6 @@ public class ChangeUserNameRequest extends BaseRequest<UserProfileResponseModel,
     public ChangeUserNameRequest(final UserNameModel _userNameModel) {
         super(UserProfileResponseModel.class, TRAServicesAPI.class);
         mNameModel = _userNameModel;
-    }
-
-    public ChangeUserNameRequest(final UserProfile _userProfile) {
-        super(UserProfileResponseModel.class, TRAServicesAPI.class);
-        mNameModel = new UserNameModel(_userProfile);
-    }
-
-    public ChangeUserNameRequest(final String _firstName, final String _lastName) {
-        super(UserProfileResponseModel.class, TRAServicesAPI.class);
-        mNameModel = new UserNameModel(_firstName, _lastName);
     }
 
     @Override
