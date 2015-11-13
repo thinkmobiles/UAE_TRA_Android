1f74635a9ab3f3491c3c5ed9d2aca90aeac23440
mobimaks <mobimaks13@gmail.com>
2015-11-13 18:13:02 +0200

    Custom ViewPager i progress

---
+++ app/src/main/java/com/uae/tra_smart_services/adapter/DynamicServicePageAdapter.java
@@ -0,0 +1,165 @@
+package com.uae.tra_smart_services.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.uae.tra_smart_services.R;
+import com.uae.tra_smart_services.entities.dynamic_service.BaseInputItem;
+import com.uae.tra_smart_services.entities.dynamic_service.InputItemsPage;
+import com.uae.tra_smart_services.entities.dynamic_service.input_item.AttachmentInputItem;
+import com.uae.tra_smart_services.interfaces.OnOpenAttachmentPickerListener;
+
+import java.lang.ref.SoftReference;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by mobimaks on 10.11.2015.
+ */
+public final class DynamicServicePageAdapter extends PagerAdapter {
+
+    private final Context mContext;
+    private final LayoutInflater mInflater;
+
+    private final String mBtnText;
+    private final List<InputItemsPage> mInputItemsPages;
+
+    private Button btnSend;
+    private SoftReference<? extends ViewGroup> mLastPageContainer;
+
+    private OnClickListener mBtnSendClickListener;
+    private OnOpenAttachmentPickerListener mAttachmentPickerListener;
+
+    public DynamicServicePageAdapter(final Context _context, final String _btnSendText) {
+        this(_context, null, _btnSendText);
+    }
+
+    public DynamicServicePageAdapter(final Context _context, final List<InputItemsPage> _inputItemsPages, final String _btnSendText) {
+        mContext = _context;
+        mInflater = LayoutInflater.from(_context);
+        mInputItemsPages = (_inputItemsPages == null) ? new ArrayList<InputItemsPage>() : _inputItemsPages;
+        mBtnText = _btnSendText;
+    }
+
+    public void setAttachmentCallback(OnOpenAttachmentPickerListener _listener) {
+        mAttachmentPickerListener = _listener;
+        for (InputItemsPage inputItemsPage : mInputItemsPages) {
+            setAttachmentCallbackIntoPage(inputItemsPage);
+        }
+    }
+
+    public void setSendButtonClickListener(@Nullable OnClickListener _btnSendClickListener) {
+        mBtnSendClickListener = _btnSendClickListener;
+        if (btnSend != null) {
+            btnSend.setOnClickListener(_btnSendClickListener);
+        }
+    }
+
+    public void addPage(InputItemsPage _page) {
+        removeOldSendBtnIfNeed();
+        mInputItemsPages.add(_page);
+        notifyDataSetChanged();
+    }
+
+    private void removeOldSendBtnIfNeed() {
+        final ViewGroup previousLastPageContainer;
+        if (mLastPageContainer != null && (previousLastPageContainer = mLastPageContainer.get()) != null) {
+            previousLastPageContainer.removeView(btnSend);
+            mLastPageContainer.clear();
+        }
+    }
+
+    private void setAttachmentCallbackIntoPage(InputItemsPage _page) {
+        for (BaseInputItem inputItem : _page.inputItems) {
+            if (inputItem.isAttachmentItem()) {
+                ((AttachmentInputItem) inputItem).setAttachmentCallback(mAttachmentPickerListener);
+            }
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mInputItemsPages.size();
+    }
+
+    @Override
+    public View instantiateItem(ViewGroup pageContainer, int position) {
+        final View pageView = mInflater.inflate(R.layout.layout_dynamic_service_page, pageContainer, false);
+        pageContainer.addView(pageView);
+
+        ViewHolder holder = (ViewHolder) pageView.getTag();
+        if (holder == null) {
+            holder = new ViewHolder(pageView);
+        }
+
+        final LinearLayout inputItemsContainer = holder.inputItemsContainer;
+        final InputItemsPage currentPage = mInputItemsPages.get(position);
+
+        final List<BaseInputItem> inputItems = currentPage.inputItems;
+        for (BaseInputItem inputItem : inputItems) {
+            View view = inputItem.getView(mInflater, inputItemsContainer);
+            inputItemsContainer.addView(view);
+        }
+
+        if (isLastItemPosition(position)) {
+            mLastPageContainer = new SoftReference<>(inputItemsContainer);
+            inputItemsContainer.addView(getSendBtn(inputItemsContainer));
+        }
+
+        setAttachmentCallbackIntoPage(currentPage);
+
+        return pageView;
+    }
+
+    private Button getSendBtn(LinearLayout _inputItemsContainer) {
+        if (btnSend == null) {
+            btnSend = (Button) mInflater.inflate(R.layout.input_item_button, _inputItemsContainer, false);
+            btnSend.setText(mBtnText);
+            btnSend.setOnClickListener(mBtnSendClickListener);
+        }
+        return btnSend;
+    }
+
+    private boolean isLastItemPosition(int position) {
+        return position == (getCount() - 1);
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        final InputItemsPage page = mInputItemsPages.get(position);
+        final ViewGroup pageView = (ViewGroup) object;
+        final LinearLayout inputItemsContainer = ((ViewHolder) pageView.getTag()).inputItemsContainer;
+        for (BaseInputItem inputItem : page.inputItems) {
+            inputItemsContainer.removeView(inputItem.getView(mInflater, inputItemsContainer));
+        }
+
+        if (isLastItemPosition(position)) {
+            inputItemsContainer.removeView(getSendBtn(inputItemsContainer));
+        }
+        container.removeView((View) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    private final class ViewHolder {
+
+        final LinearLayout inputItemsContainer;
+
+        public ViewHolder(@NonNull final View _view) {
+            inputItemsContainer = (LinearLayout) _view.findViewById(R.id.pllContainer_LDSP);
+            _view.setTag(this);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/uae/tra_smart_services/customviews/MyViewPager.java
@@ -0,0 +1,219 @@
+package com.uae.tra_smart_services.customviews;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.animation.DecelerateInterpolator;
+
+import com.uae.tra_smart_services.util.Logger;
+
+import hugo.weaving.DebugLog;
+
+/**
+ * Created by mobimaks on 11.11.2015.
+ */
+
+public class MyViewPager extends ViewPager {
+
+    private static final float NEW_PAGE_SCROLL_OFFSET = 0.35f;
+
+    private GestureDetectorCompat mDetector;
+    private boolean mIsFlingHandled;
+
+    public MyViewPager(Context context) {
+        this(context, null);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mDetector = new GestureDetectorCompat(getContext(), new SwipeListener());
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (mIsFlingHandled) {
+            return true;
+        }
+        Logger.d(MyViewPager.class.getSimpleName(), "⇠ dispatchTouchEvent [0ms] = " + mDetector.onTouchEvent(ev));
+        handleScrollPositionIfNeed(ev);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @DebugLog
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (mIsFlingHandled) {
+            return true;
+        }
+        Logger.d(MyViewPager.class.getSimpleName(), "⇠ onTouchEvent [0ms] = " + mDetector.onTouchEvent(ev));
+        handleScrollPositionIfNeed(ev);
+        return true;
+    }
+
+
+    private void handleScrollPositionIfNeed(MotionEvent ev) {
+        if (!mIsFlingHandled && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_CANCEL)) {
+            final int pageWidth = getWidth();
+            final int currentPageStartX = getCurrentPageX();
+            final int scrollX = getScrollX();
+
+            if (canOpenPreviousPage() && (scrollX < currentPageStartX - pageWidth * NEW_PAGE_SCROLL_OFFSET)) {
+                mIsFlingHandled = true;
+                openPreviousPage();
+                //openPreviousPage
+            } else if (canOpenNextPage() && (scrollX > currentPageStartX + pageWidth * NEW_PAGE_SCROLL_OFFSET)) {
+                mIsFlingHandled = true;
+                openNextPage();
+                //openNextPage
+            } else if (getScrollX() != currentPageStartX) {
+                mIsFlingHandled = true;
+                ValueAnimator animator = ValueAnimator.ofInt(getScrollX(), currentPageStartX);
+                animator.setDuration(300);
+                animator.setInterpolator(new DecelerateInterpolator());
+                animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        scrollTo((Integer) animation.getAnimatedValue(), 0);
+                    }
+                });
+                animator.start();
+            }
+        }
+    }
+
+    @DebugLog
+    @Override
+    protected void onScrollChanged(final int _left, final int _top, final int _oldLeft, final int _oldTop) {
+        super.onScrollChanged(_left, _top, _oldLeft, _oldTop);
+        if (_left == getCurrentPageX()) {
+            Logger.d(this, "onScrollChanged: " + _left);
+            mIsFlingHandled = false;
+        }
+    }
+
+    private int getCurrentPageX() {
+        return getCurrentItem() * getWidth();
+    }
+
+    private class SwipeListener extends SimpleOnGestureListener {
+
+        private static final float FLING_START_THRESHOLD = 0.15f;
+        private static final float MAX_SCROLL_OFFSET = 1.0f;
+        private static final float BLOCK_SCROLL_OFFSET = 0.1f;
+
+        @DebugLog
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            if (e1 != null && e2 != null) {
+                if (isFlingBigEnough(e1, e2)) {
+                    if (e1.getX() < e2.getX()) {
+                        return onSwipeRight(velocityX, velocityY);
+                    } else {
+                        return onSwipeLeft(velocityX, velocityY);
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean isFlingBigEnough(MotionEvent e1, MotionEvent e2) {
+            return Math.abs(e1.getX() - e2.getX()) / getWidth() >= FLING_START_THRESHOLD;
+        }
+
+        @DebugLog
+        public boolean onSwipeLeft(float velocityX, float velocityY) {
+            if (canOpenNextPage()) {
+                Logger.d(this, "onFling Next");
+                mIsFlingHandled = true;
+                openNextPage();
+                return true;
+            }
+            return false;
+        }
+
+        @DebugLog
+        public boolean onSwipeRight(float velocityX, float velocityY) {
+            if (canOpenPreviousPage()) {
+                Logger.d(this, "onFling Previous");
+                mIsFlingHandled = true;
+                openPreviousPage();
+                return true;
+            }
+            return false;
+        }
+
+
+        @DebugLog
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            final int currentScrollX = getScrollX();
+            final int currentPageStartX = getCurrentPageX();
+            final float maxScrollOffset = getWidth() * MAX_SCROLL_OFFSET;
+
+            final boolean canScrollLeft = (currentScrollX > 0) && (currentScrollX > currentPageStartX - maxScrollOffset);
+            final boolean canScrollRight = (currentScrollX < getLastPageX()) && (currentScrollX < currentPageStartX + maxScrollOffset);
+
+            if (distanceX < 0 && canScrollLeft) {
+                scrollBy(Math.round(distanceX), 0);
+                Logger.d(MyViewPager.class.getSimpleName(), "Scroll action: onScroll left");
+                return true;
+            } else if (distanceX > 0 && canScrollRight) {
+                scrollBy(Math.round(distanceX), 0);
+                Logger.d(MyViewPager.class.getSimpleName(), "Scroll action: onScroll right");
+                return true;
+            }
+
+            return false;
+        }
+
+        private int getLastPageX() {
+            return getWidth() * (getAdapter().getCount() - 1);
+        }
+
+        @DebugLog
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Logger.d(MyViewPager.class.getSimpleName(), "Touch action: onDown");
+            return true;
+        }
+
+    }
+
+    private boolean canOpenNextPage() {
+        return getCurrentItem() < getAdapter().getCount() - 1;
+    }
+
+    private boolean canOpenPreviousPage() {
+        return getCurrentItem() > 0;
+    }
+
+    private void openPreviousPage() {
+        setCurrentItem(getCurrentItem() - 1);
+    }
+
+    private void openNextPage() {
+        setCurrentItem(getCurrentItem() + 1);
+    }
+
+    private String getActionName(int action) {
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                return "ACTION_DOWN";
+            case MotionEvent.ACTION_UP:
+                return "ACTION_UP";
+            case MotionEvent.ACTION_MOVE:
+                return "ACTION_MOVE";
+            case MotionEvent.ACTION_CANCEL:
+                return "ACTION_CANCEL";
+            case MotionEvent.ACTION_SCROLL:
+                return "ACTION_SCROLL";
+            default:
+                return "Other action";
+        }
+    }
+
+}
